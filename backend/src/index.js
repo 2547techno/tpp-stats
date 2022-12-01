f29aa49405deb665d9e6793aee40208681112222
2547techno <technowizz2547@gmail.com>
2022-12-01 02:03:21 -0500

    update stats in db

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,15 +6,20 @@ if (process.env.DOTENV) {
     require("dotenv").config();
 }
 
-const pool = mariadb.createPool({host: process.env.DB_HOST, user: process.env.DB_USER, connectionLimit: 5});
+const pool = mariadb.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE,
+    connectionLimit: 1
+});
 const client = new ChatClient();
-const updateQueue = new Queue();
+// const updateQueue = new Queue();
 const STAT_KEYWORDS = ["left","right","up","down","a","b","start","select","anarchy","democracy"];
 
-updateQueue.on("push", val => {
-    console.log(`push: ${JSON.stringify(val)}`);
-    console.log(`size: ${updateQueue.size()}`);
-})
+// updateQueue.on("push", val => {
+//     console.log(`push: ${JSON.stringify(val)} | size: ${updateQueue.size()}`);
+// })
 
 client.on("ready", () => {
     console.log("[IRC] Ready");
@@ -26,7 +31,7 @@ client.on("close", err => console.log(`[IRC] Closed: ${err}`))
 client.on("JOIN", ({channelName}) => console.log(`[IRC] Joined: ${channelName}`))
 
 client.on("PRIVMSG", (msg) => {
-    console.log(`[#${msg.channelName}] ${msg.displayName}: ${msg.messageText}`);
+    // console.log(`[#${msg.channelName}] ${msg.displayName}: ${msg.messageText}`);
     if (msg.channelID == process.env.TARGET_CHANNEL) {
         updateStat(msg)
     }
@@ -34,11 +39,44 @@ client.on("PRIVMSG", (msg) => {
 
 function updateStat({senderUserID, displayName, messageText}) {
     if (STAT_KEYWORDS.includes(messageText.trim().toLowerCase())) {
-        updateQueue.push({
-            uid: senderUserID,
+        // updateQueue.push({
+        //     uid: senderUserID,
+        //     username: displayName,
+        //     message: messageText.trim().toLowerCase()
+        // })
+
+        updateDb({
+            uid: parseInt(senderUserID),
             username: displayName,
-            message: messageText.trim().toLowerCase()
+            stat: messageText.trim().toLowerCase()
         })
+    }
+}
+
+async function updateDb({uid, username, stat}) {
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        conn.beginTransaction();
+        const res = await conn.query(`
+            UPDATE TPP_STATS.STATS
+            SET COUNT_${stat.toUpperCase()} = COUNT_${stat.toUpperCase()}+1
+            WHERE UID=${uid};
+        `);
+
+        if (res.affectedRows < 1) {
+            await conn.query(`
+                INSERT INTO TPP_STATS.STATS
+                (UID, USERNAME, COUNT_${stat.toUpperCase()})
+                VALUES(${uid}, '${username}', 1);
+            `);
+        }
+        conn.commit();
+    } catch(err) {
+        console.error(err);
+        conn.rollback();
+    } finally {
+        if (conn) conn.release();
     }
 }
 
